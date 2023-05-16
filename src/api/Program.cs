1a0a9cc2f1b030a853ab346b8f7f351d6e90329b
Randy Jerome <rjerome@xpirit.com>
2023-05-16 12:39:19 -0400

    Updated swagger gen

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,11 +1,22 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(e =>
+{
+    e.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Yacht Racing API"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    e.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
