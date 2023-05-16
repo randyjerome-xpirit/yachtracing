e8205a25f5777fb715a907b9ca4da213b0c52341
Randy Jerome <rjerome@xpirit.com>
2023-05-16 10:10:16 -0400

    Updated swagger to use default

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -10,11 +10,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI(e => e.SwaggerEndpoint("/swagger/swagger.json", "Yacht Racing API"));
+
 
 app.UseHttpsRedirection();
 
