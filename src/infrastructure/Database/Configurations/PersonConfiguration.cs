f33ae30562e282b905e771fec1db46809efa3130
Randy Jerome <rjerome@xpirit.com>
2023-05-16 09:17:26 -0400

    Removed the dev requirement in startup for the swagger load. Adding some more entities

---
+++ src/infrastructure/Database/Configurations/PersonConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using domain.entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.Database.Configurations;
+public sealed class PersonConfiguration : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("people");
+        builder.Property(x => x.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+        builder.Property(x => x.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
