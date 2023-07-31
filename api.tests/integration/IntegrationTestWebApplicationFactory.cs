b8d068027ea2a5b520f032ea717ff894062caa9e
Randy Jerome <rjerome@xpirit.com>
2023-07-31 13:31:47 -0400

    Converted yachtclubcontroller to use DI and added unit tests

---
+++ api.tests/integration/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using infrastructure.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+
+namespace api.tests.integration;
+public class IntegrationTestWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(svcs =>
+        {
+            var db = svcs.SingleOrDefault(e => e.ServiceType == typeof(DbContextOptions<YachtDbContext>));
+            if (db != null)
+            {
+                svcs.Remove(db);
+            }
+
+            var dbConn = svcs.SingleOrDefault(e => e.ServiceType == typeof(DbConnection));
+            if (dbConn != null)
+            {
+                svcs.Remove(dbConn);
+            }
+
+            svcs.AddSingleton<DbConnection>(e =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            svcs.AddDbContext<YachtDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+        builder.UseEnvironment("Development");
+
+    }
+}
---
+++ api.tests/unit/Controllers/YachtClubControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using api.Controllers;
+using domain.entities;
+using infrastructure.tests.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.tests.unit.Controllers;
+
+
+public class YachClubControllerUnitTestDbContext : UnitTestDbContext
+{
+    protected override IEnumerable<YachtClub> GenerateYachtClubs() =>
+        new YachtClub[]
+        {
+            new YachtClub
+            {
+                Id = 1,
+                Address1 = "123 some street",
+                Address2 = null,
+                City = "some city",
+                State = "XX",
+                Country = "USA",
+                Name = "Some yacht club",
+                PostalCode = "12345"
+
+            }
+        };
+
+}
+
+
+public abstract class YachtClubControllerTests
+{
+    protected YachtClubController SystemUnderTest { get; set; }
+    public YachtClubControllerTests()
+    {
+        var db = new YachClubControllerUnitTestDbContext();
+        db.Database.EnsureCreated();
+        this.SystemUnderTest = new YachtClubController(db);
+    }
+}
+
+[TestClass]
+public class WhenGetIsCalled : YachtClubControllerTests
+{
+    [TestMethod]
+    public async Task ThenAllRecordsAreReturned()
+    {
+        //arrange
+
+        //act
+        var eval = await SystemUnderTest.Get();
+
+        //assert
+        eval.Should().BeOfType<OkObjectResult>();
+        ((OkObjectResult)eval).Value.Should().NotBeNull();
+        ((IEnumerable<YachtClub>)((OkObjectResult)eval).Value).Should().HaveCount(1);
+
+    }
+
+
+}
+
